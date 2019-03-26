2269de498cb1ec471d5f7be2c755e1047a96e75c
puochan <jiangxueyx007@live.com>
2019-03-27 00:06:45 +0800

    update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
-nohup /gost -L socks5://:1080 2>&1 > /dev/null &
-/bridge server 8080 $URL $AUTH
+nohup /bridge server 8000 $BURL $BAUTH 2>&1 > /dev/null &
+/gost -L socks5://:1080 -L=http://$GOSTUSER:$GOSTPASSWD@:8080?probe_resist=host:127.0.0.1:8000  > /dev/null &
